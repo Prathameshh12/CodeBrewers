5c17c5ae46e6c7501fb68ab9e9a5e6214de1bc9c
Prathamesh Ahire <prathamesh_ahire@MacBook-Air-84.local>
2025-06-01 20:27:28 +1000

    Initial commit

---
+++ CodeBrewersFocus/CodeBrewersFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBrewersFocusApp.swift
+//  CodeBrewersFocus
+//
+//  Created by Prathamesh Ahire on 1/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeBrewersFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBrewersFocusTests/CodeBrewersFocusTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBrewersFocusTests.swift
+//  CodeBrewersFocusTests
+//
+//  Created by Prathamesh Ahire on 1/6/2025.
+//
+
+import Testing
+@testable import CodeBrewersFocus
+
+struct CodeBrewersFocusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
