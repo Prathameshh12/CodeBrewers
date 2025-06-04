9155b4f523b9fffdd40761d8516922bb17b57880
Megha <meghaelizageorge@gmail.com>
2025-06-04 11:04:13 +1000

    Reflection View 2

--- CodeBrewersFocus/Views/ReflectionsView.swift
+++ CodeBrewersFocus/Views/ReflectionsView.swift
@@ -1,135 +1,57 @@
-
+//
+//  ContentView.swift
+//  reflectionsPage
+//
+//  Created by Megha Elisa George on 30/5/2025.
+//
 import SwiftUI
 
-struct ReflectionsView: View{
-    @State private var reflectionText = ""
-    @State private var showConfirmation = false
-    @FocusState private var isTextEditorFocused: Bool
-    
+struct FavoriteItem: Identifiable {
+    let id = UUID()
+    let imageName: String
+    let name: String
+    let desc: String
+    let date: Date
+}
+struct ReflectionsView: View {
+    @State private var items: [FavoriteItem] = [
+        FavoriteItem(imageName: "tree", name: "Tree", desc: "Resilience & Growth", date: Date()),
+        FavoriteItem(imageName: "icecream",name: "Ice Cream", desc: "Living Moments & Letting Go", date: Date()),
+        FavoriteItem(imageName: "heart", name: "Heart", desc: "Centeredness & Steadiness", date: Date()),
+        FavoriteItem(imageName: "frog", name: "Frog", desc: "Leap & Change",date: Date()),
+        FavoriteItem(imageName: "happy", name: "Happy Face", desc: "Calm & Positivity",date: Date()),
+        FavoriteItem(imageName: "strawberry", name: "Strawberyy", desc: "Nourishment & Vitality",date: Date())
+    ]
     var body: some View {
-        
-        VStack(alignment: .leading) {
-            // Top bar
-            HStack {
-                Image(systemName: "chevron.backward")
-                    .font(.system(size: 25))
-                    .foregroundColor(.gray)
-                Spacer()
-                Text("Reflection")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                Spacer()
-                Image(systemName: "xmark")
-                    .font(.system(size: 25))
-                    .foregroundColor(.gray)
-                    }
-            .padding(.horizontal)
-            .padding(.top, 0)
-
-            // Title with icon
-            HStack(alignment: .top, spacing: 8) {
-                Image(systemName: "bubbles.and.sparkles.fill")
-                    .font(.largeTitle)
-                Text("Tree and focus")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    }
-            .padding(.horizontal)
-            .padding(.top, 2)
-            
-            // Reflection input
-            ZStack(alignment: .topLeading){
-                TextEditor(text: $reflectionText)
-                    .frame(minHeight: 150)
-                    .padding(.horizontal)
-                    .padding(.top, 0)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.clear, lineWidth: 1)
-                            )
-                    .focused($isTextEditorFocused)
-
-                
-                if reflectionText.isEmpty {
-                    Text("Reflect on the relationship between your creation and focus")
-                        .foregroundColor(.gray)
-                        .padding(.horizontal, 20)
-                        .padding(.top, 2)
-                        .onTapGesture {
-                            isTextEditorFocused = true
-                                    }
-                        }
-                    }
-            .contentShape(Rectangle())
-            .onTapGesture {
-                isTextEditorFocused = true
-            }
-            
-            // Save Button
-            Button(action: {
-                isTextEditorFocused = false
-                withAnimation {
-                    showConfirmation = true
-                }
-            }) {
-                Text("Save")
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(reflectionText.isEmpty ? Color.gray.opacity(0.2) : Color.blue)
-                    .foregroundColor(reflectionText.isEmpty ? .gray : .white)
-                    .cornerRadius(25)
-            }
-            .padding(.horizontal)
-            .padding(.top, 10)
-            .disabled(reflectionText.isEmpty)        }
-        .padding()
-        .overlay(
-            Group {
-                if showConfirmation {
-                    VStack(spacing: 0) {
-                        VStack(spacing: 12) {
-                            Image(systemName: "checkmark.circle.fill")
-                                .font(.system(size: 30))
-                                .foregroundColor(.blue)
-
-                            Text("Reflection saved")
-                                .font(.headline)
-                                .bold()
-
-                            Text("Your reflections\nare yours to revisit anytime.")
-                                .font(.subheadline)
-                                .multilineTextAlignment(.center)
-                        }
-                        .padding()
-
-                        Divider()
-
-                        Button(action: {
-                            withAnimation {
-                                showConfirmation = false
+        NavigationView {
+            Form{
+                    ForEach($items) { $item in
+                        HStack(spacing : 10){
+                            Image(item.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 70, height: 70)
+                            VStack (alignment: .leading){
+                                Spacer()
+                                Text(item.name)
+                                Text(item.desc)
+                                    .foregroundColor(.gray)
+                                Text(item.date, style: .date)
+                                    .foregroundColor(.gray)
+                                    .font(.footnote)
+                                Spacer()
                             }
-                        }) {
-                            Text("Cool!")
-                                .font(.body)
-                                .fontWeight(.semibold)
-                                .foregroundColor(.blue)
-                                .frame(maxWidth: .infinity)
-                                .padding(12)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .padding(.horizontal)
+                                .foregroundColor(.gray)
                         }
                     }
-                    .background(Color.white)
-                    .cornerRadius(16)
-                    .padding(.horizontal, 40)
-                    .shadow(radius: 10)
-                    .transition(.scale)
-                }
             }
-        )
-
+            .navigationTitle("Reflections")
+        }
     }
 }
-
 #Preview {
     ReflectionsView()
 }
